0fcab5344075a3aeead2e8399ae59fff604f703a
mbendohhoulaouini <mohammed.bendohhou.laouini@alumnos.upm.es>
2021-04-25 19:19:53 +0200

    Refactor "Rename variables"

--- src/main/java/usantatecla/Intersection.java
+++ src/main/java/usantatecla/Intersection.java
@@ -2,29 +2,29 @@ package usantatecla;
 
 public class Intersection {
 
-    private Interval a;
-    private Interval b;
+    private Interval intervalOne;
+    private Interval intervalTwo;
 
 
-    public Intersection(Interval a, Interval b) {
-        this.a=a;
-        this.b=b;
+    public Intersection(Interval intervalOne, Interval intervalTwo) {
+        this.intervalOne=intervalOne;
+        this.intervalTwo=intervalTwo;
     }
 
-    public Interval calculateIntersection(Interval a, Interval b){
+    public Interval calculateIntersection(Interval intervalOne, Interval intervalTwo){
         Interval result = null;
-        if(this.compareIntervals(a, b)){
-            Min min=(a.getMin().value >= b.getMin().value) ? a.getMin(): b.getMin();
-            Max max=(a.getMax().value <= b.getMax().value) ? a.getMax() : b.getMax();
+        if(this.compareIntervals(intervalOne, intervalTwo)){
+            Min min=(intervalOne.getMin().value >= intervalTwo.getMin().value) ? intervalOne.getMin(): intervalTwo.getMin();
+            Max max=(intervalOne.getMax().value <= intervalTwo.getMax().value) ? intervalOne.getMax() :intervalTwo.getMax();
             result = new Interval(min, max);
         }
 
         return result;
     }
 
-    public boolean compareIntervals(Interval a, Interval b)
+    public boolean compareIntervals(Interval intervalOne, Interval intervalTwo)
     {
-        return Math.max(a.getMin().value, b.getMin().value)<=Math.min(a.getMax().value, this.b.getMax().value);
+        return Math.max(intervalOne.getMin().value, intervalTwo.getMin().value)<=Math.min(intervalOne.getMax().value, intervalTwo.getMax().value);
     }
 
 }
